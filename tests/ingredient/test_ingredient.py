6e23e3f7e82b51a171a04fa5919caa479c6b0b09
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2023-06-16 15:25:07 -0300

    test: req 01 done

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,17 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Restriction  # noqa: F401, E261, E501
+
+mock_carne = {Restriction.ANIMAL_MEAT, Restriction.ANIMAL_DERIVED}
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient = Ingredient("carne")
+    ingredient2 = Ingredient("farinha")
+    assert ingredient.restrictions == mock_carne
+    assert ingredient.name == "carne"
+    assert repr(ingredient) == "Ingredient('carne')"
+    assert ingredient == ingredient
+    assert ingredient != ingredient2
+    assert hash(ingredient) == hash(ingredient)
+    assert hash(ingredient) != hash(ingredient2)
